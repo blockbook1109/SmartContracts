b1a8a259b3ab1fd153edde255415836db363fbd3
blockbook1109 <72395495+blockbook1109@users.noreply.github.com>
2020-10-05 22:52:45 +0530

    added simple contract

---
+++ showName.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.7.0;
+
+contract showName {
+
+
+    function addName(string name){
+        return name;
+    }
+
+
+    function display(string name) public returns {
+        return name;
+
+    }
+
+}
